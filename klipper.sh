ec14de54cf4ddc2d0a71305f241f14e0301bf56a
pointdnd <point.dnd@gmail.com>
2020-05-10 18:51:47 +0300

    Create klipper.sh

---
+++ klipper.sh
@@ -0,0 +1,14 @@
+echo "Installing Klipper ..."
+sudo apt-get install python3-pip --yes
+cd~
+git clone https://github.com/KevinOConnor/klipper
+./klipper/scripts/install-octopi.sh
+cd ~/klipper/
+sudo service klipper stop
+echo "-----------------------------------------------------------------------------------------------------------------"
+echo ""
+echo "Communication ports list"
+echo ""
+ls /dev/serial/by-id/*
+echo ""
+echo "See http://3dua.info for more information"
