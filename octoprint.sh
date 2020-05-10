274074786efd207f1a3e1844f0d63138d89a2710
Denis Ignatenko <60291791+test3210-d@users.noreply.github.com>
2020-05-10 18:56:16 +0300

    Create octoprint.sh

---
+++ octoprint.sh
@@ -0,0 +1,14 @@
+cd ~
+sudo apt-get install python-pip python-dev python-setuptools python-virtualenv git libyaml-dev build-essential
+git clone https://github.com/foosel/OctoPrint.git
+cd OctoPrint
+virtualenv --system-site-packages venv
+./venv/bin/python setup.py install
+mkdir ~/.octoprint
+sudo usermod -a -G tty pi
+sudo usermod -a -G dialout pi
+~/OctoPrint/venv/bin/octoprint
+sudo cp ~/OctoPrint/scripts/octoprint.init /etc/init.d/octoprint
+sudo chmod +x /etc/init.d/octoprint
+sudo cp ~/OctoPrint/scripts/octoprint.default /etc/default/octoprint
+sudo nano /etc/default/octoprint
